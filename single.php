8319f0d189005153f384c2a0668fb95e3f4e340c
Richi <generator@mydevelops.com>
2017-12-07 14:24:06 +0100

    NEW: support for posts

--- single.php
+++ single.php
@@ -1,37 +1,67 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying single post
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
  */
 
-get_header(); ?>
+get_header();
+the_post();
+?>
+<?php if ( has_post_thumbnail()) : ?>
+    <style>
+        .triangel{
+            visibility:none;
+        }
+        .inner .halbe{
+            right: 0px !important;
+            left: 0px !important;
+        }
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/post/content', get_post_format() );
+    </style>
+<?php endif; ?>
+	<article id="<?php  global $post; echo $post->post_name; ?>"  >
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
 
-				the_post_navigation( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'semantify' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'semantify' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">'  . '</span>%title</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'semantify' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'semantify' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' .  '</span></span>',
-				) );
 
-			endwhile; // End of the loop.
-			?>
+		<div class="container">
+			<div class="section">
+				<!--   Icon Section   -->
 
-	<?php get_sidebar(); ?>
+                <?php if ( has_post_thumbnail()) : ?>
+                        <div class="post-image big" style="background-image: url('<?php echo the_post_thumbnail_url( 'large' );  ?>');" ></div></a>
+                <?php endif; ?>
+
+
+				<div class="row">
+					<div class="col s12">
+						<div class="nadpis center">
+                            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						</div>
+						<div class="claim-box center-align">
+							<div class="claim-text ">
+								<p>
+                                    <?php echo get_post_meta($post->ID, 'perex',1); ?>
+								</p>
+							</div>
+							<div class="page-text">
+                                <?php
+                                /* translators: %s: Name of current post */
+                                the_content();
+                                ?>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+
+		</div>
+
+	</article>
+
+
 
 
 <?php get_footer();
--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -12,40 +12,62 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-	if ( is_sticky() && is_home() ) :
-		echo semantify_get_svg( array( 'icon' => 'thumb-tack' ) );
-	endif;
-	?>
-	<header class="entry-header">
 
-	</header><!-- .entry-header -->
 
-	<?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
-		<div class="post-thumbnail">
-			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail( 'semantify-featured-image' ); ?>
-			</a>
-		</div><!-- .post-thumbnail -->
-	<?php endif; ?>
+<article id="post-<?php the_ID(); ?>" class="post" >
 
-	<div class="entry-content">
-		<?php
-		/* translators: %s: Name of current post */
-		the_content( sprintf(
-			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'semantify' ),
-			get_the_title()
-		) );
 
-		wp_link_pages( array(
-			'before'      => '<div class="page-links">' . __( 'Pages:', 'semantify' ),
-			'after'       => '</div>',
-			'link_before' => '<span class="page-number">',
-			'link_after'  => '</span>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-    
 
-</article><!-- #post-## -->
+    <div class="container">
+        <div class="section">
+            <!--   Icon Section   -->
+
+
+
+            <div class="row">
+
+                    <?php if ( has_post_thumbnail()) : ?>
+                        <div class="col m4 s12 l3">
+                            <a href="<?php echo get_permalink(); ?>"><div class="post-image" style="background-image: url('<?php echo the_post_thumbnail_url( 'small' );  ?>');" ></div></a>
+                        </div>
+                        <div class="col m8 s12 l9">
+                    <?php else: ?>
+
+                            <div class="col s12 ">
+                    <?php endif; ?>
+                    <div class="nadpis">
+                        <a href="<?php echo get_permalink(); ?>"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
+                    </div>
+                    <div class="meta">
+                        <?php the_date(); ?> by <?php the_author(); ?>
+                    </div>
+                        <div class="claim-text ">
+                            <p>
+                                <?php echo get_post_meta($post->ID, 'perex',1); ?>
+                            </p>
+                        </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col 12">
+                    <div class="page-text">
+                        <?php
+                        /* translators: %s: Name of current post */
+                        wp_link_pages( array(
+                                           'before'      => '<div class="page-links">' . __( 'Pages:', 'semantify' ),
+                                           'after'       => '</div>',
+                                           'link_before' => '<span class="page-number">',
+                                           'link_after'  => '</span>',
+                                       ) );
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+</article>
+
